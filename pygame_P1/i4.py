7dd9e2c06babdd424831e38078d4c0f31a714914
Matthew O'Rourke <mwk.orourke@hotmail.com>
2021-03-08 15:33:19 +0000

    now attempts to simulate physics (badly)

--- pygame_P1/i4.py
+++ pygame_P1/i4.py
@@ -8,28 +8,29 @@ screen = pygame.display.set_mode((1080,720), RESIZABLE)
 pygame.display.set_caption('test window')
 
 body_list = []
-scale_factor = 1
+scale_factor = 5
 
 screen_size = [1080,720]
 
-planet_one = Body(100, [0,0], [0,0],'star.png')
+planet_one = Body(10000e12, [0,0], [0,0],'circle.png')
 body_list.append(planet_one)
-#planet_two = Body(pos=[50,50], image='circle.png')
-#body_list.append(planet_two)
+planet_two = Body(10000, [0,0], [5000,0],'star.png')
+body_list.append(planet_two)
 
-
-
+i =1
 while True:
-
-    for item in body_list:
-        screen.blit(item.image, item.screen_pos)
-    for item in body_list:
-        item.update_screen_pos([0,0],scale_factor,screen_size)
+    if i == 1000:
+        i = 0
+        for item in body_list:
+           screen.blit(item.image, item.screen_pos)
+           item.update_screen_pos([0,0],scale_factor,screen_size)
+           pygame.display.update()
+    for index, item in enumerate(body_list):
+        item.calculate_movement(body_list[index+1:])
     for event in pygame.event.get():    # fetches event queue
         if event.type == QUIT:          # checks if the window has been closed    
             quit()                      # exits the program.
         if event.type == VIDEORESIZE:
             screen_size = pygame.display.get_window_size()
             print(screen_size)
-
-    pygame.display.update()
+    i += 1
--- pygame_P1/i4_classes.py
+++ pygame_P1/i4_classes.py
@@ -13,26 +13,26 @@ class Body():
     
     def _calculate_force(self, object_list, phys_step = 1, G = 6.67408e-11):
         for body in object_list:
-            dist = sqrt((self._pos[0] - body.pos[0])**2 + (self._pos[1] - body.pos[1])**2)
+            dist = sqrt((self._pos[0] - body._pos[0])**2 + (self._pos[1] - body._pos[1])**2)
             temp_force = [0,0]
             linear_force = G * (self._mass + body._mass) / (dist**2)
-            temp_force[0] = (self._pos[0] - body.pos[0]) / dist * linear_force
-            temp_force[1] = (self._pos[1] - body.pos[1]) / dist * linear_force
+            temp_force[0] = (self._pos[0] - body._pos[0]) / dist * linear_force
+            temp_force[1] = (self._pos[1] - body._pos[1]) / dist * linear_force
         
-            if self._pos[0] > body.pos[0]:
+            if self._pos[0] > body._pos[0]:
                 self._force[0] -= abs(temp_force[0])
                 body._force[0] += abs(temp_force[0])
             else:
                 self._force[0] += abs(temp_force[0])
                 body._force[0] -= abs(temp_force[0])
-            if self._pos[1] < body.pos[1]:
+            if self._pos[1] < body._pos[1]:
                 self._force[1] -= abs(temp_force[1])
                 body._force[1] += abs(temp_force[1])
             else:
                 self._force[1] += abs(temp_force[1])
                 body._force[1] -= abs(temp_force[1])
         
-    def _apply_force(self):
+    def _apply_force(self,phys_step):
         accel = [(self._force[0]/self._mass),(self._force[0]/self._mass)]
 
         self._pos[0] += self._vel[0] + (accel[0] * phys_step/2)
@@ -45,7 +45,7 @@ class Body():
     
     def calculate_movement(self, object_list, phys_step = 1, G = 6.67408e-11):
         self._calculate_force(object_list, phys_step)
-        self._apply_force()
+        self._apply_force(phys_step)
 
     def update_screen_pos(self, screen_offset, scale_factor, screen_size):
         self.screen_pos[0] = round(((self._pos[0] - screen_offset[0])/(scale_factor**2)) + screen_size[0]/2)
