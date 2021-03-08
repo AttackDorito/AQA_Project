1c8ad678b9b09fbf291b06a040104057bba968ec
Matthew <mwk.orourke@hotmail.com>
2021-03-08 14:18:26 +0000

    resizable window added

--- pygame_P1/i4.py
+++ pygame_P1/i4.py
@@ -1,12 +1,16 @@
+from pygame.constants import RESIZABLE, VIDEORESIZE
 import pygame.display
 from pygame.locals import QUIT
 from i4_classes import Body
 
 
-screen = pygame.display.set_mode((1080,720))        
+screen = pygame.display.set_mode((1080,720), RESIZABLE)        
 pygame.display.set_caption('test window')
 
 body_list = []
+scale_factor = 10000
+
+screen_size = [1080,720]
 
 planet_one = Body(pos=[700,300], image='star.png')
 body_list.append(planet_one)
@@ -14,10 +18,16 @@ planet_two = Body(pos=[100,100], image='circle.png')
 body_list.append(planet_two)
 
 
+
 while True:
     for item in body_list:
         screen.blit(item.image, item.screen_pos)
+        for item in body_list:
+            item.update_screen_pos([0,0],scale_factor,screen_size)
     for event in pygame.event.get():    # fetches event queue
         if event.type == QUIT:          # checks if the window has been closed    
             quit()                      # exits the program.
+        if event.type == VIDEORESIZE:
+            screen_size = pygame.display.get_window_size()
+
     pygame.display.update()
--- pygame_P1/i4_classes.py
+++ pygame_P1/i4_classes.py
@@ -2,18 +2,17 @@ import pygame.display
 from math import sqrt
 
 class Body():
-    def __init__(self,mass = 1000, vel = [0,0], pos=[0,0], image='star.png', list_index=0):
+    def __init__(self,mass = 1000, vel = [0,0], pos=[0,0], image='star.png'):
         self.screen_pos = pos
         self.image = pygame.image.load(image)
-        self._list_index = list_index
         self._mass = mass
         self._pos = pos
         self._vel = vel
         self._force = [0,0]
 
     
-    def calculate_movement(self, object_list, phys_step = 1, G = 6.67408e-11):
-        for body in object_list[self._list_index+1:]:
+    def _calculate_force(self, object_list, phys_step = 1, G = 6.67408e-11):
+        for body in object_list:
             dist = sqrt((self._pos[0] - body.pos[0])**2 + (self._pos[1] - body.pos[1])**2)
             temp_force = [0,0]
             linear_force = G * (self._mass + body._mass) / (dist**2)
@@ -33,6 +32,7 @@ class Body():
                 self._force[1] += abs(temp_force[1])
                 body._force[1] -= abs(temp_force[1])
         
+    def _apply_force(self):
         accel = [(self._force[0]/self._mass),(self._force[0]/self._mass)]
 
         self._pos[0] += self._vel[0] + (accel[0] * phys_step/2)
@@ -42,4 +42,11 @@ class Body():
         self._vel[1] += accel[1] * phys_step
 
         self._force = [0,0]
+    
+    def calculate_movement(self, object_list, phys_step = 1, G = 6.67408e-11):
+        self._calculate_force(object_list, phys_step)
+        self._apply_force()
 
+    def update_screen_pos(self, screen_offset, scale_factor, screen_size):
+        self.screen_pos[0] = ((self._pos[0] - screen_offset[0])/scale_factor**2)+ screen_size[0]/2
+        self.screen_pos[1] = ((self._pos[1] - screen_offset[1])/scale_factor**2) + screen_size[1]/2
